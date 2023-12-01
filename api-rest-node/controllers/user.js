bd6403943ca1d1e103353a690a85498cb0f56613
=jhreyes483 <=jhreyes483@misena.edu.co>
2023-12-01 16:09:53 -0600

    se agrega libreria para run server en modo dev

--- api-rest-node/controllers/user.js
+++ api-rest-node/controllers/user.js
@@ -1,5 +1,7 @@
 'use strict'
 
+var validator = require('validator');
+
 var controller = {
     probando: function(req, res){
         return res.status(200).send({
@@ -10,7 +12,35 @@ var controller = {
         return res.status(200).send({
             menssage: "Soy el motodo TESTEANDO"
         });
+    },
+    save: function(req, res){
+        // Recoger los parametros de la peticion
+        var params = req.body;
+
+        // Validar los datos
+        var validate_name     = validator.isEmpty(params.name);
+        var validate_surname  = validator.isEmpty(params.name);
+        var validate_emial    = validator.isEmpty(params.email) && validator.isEmail(params.email);
+        var validate_password = validator.isEmpty(params.password);
+        console.log(validate_name , validate_surname, validate_emial, validate_password);
+
+        // Crear objeto de usuario
+
+        // Asignar valores al usuario
+
+        // Comprobar si el usuario ya existe 
+
+        // Si no existe, cifrar la contraseña y guradarlo
+
+        //Devovler una respuesta
+
+        return res.status(200).send({
+            message: "Registro de usuarios",
+            params
+
+        });
     }
+
 };
 
 
--- api-rest-node/routes/user.js
+++ api-rest-node/routes/user.js
@@ -5,7 +5,11 @@ var UserController = require('../controllers/user');
 
 var router         = express.Router();
 
+/**************** Rutas de prueba ********************* */
 router.get('/probando',UserController.probando);
 router.post('/testeando',UserController.testeando);
+/****************************************************** */
+/****************** Rutas de usuario ****************** */
+router.post('/register', UserController.save);
 
 module.exports = router;
