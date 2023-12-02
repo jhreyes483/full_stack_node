201d57953d95f6df339b1958174b61b4a2fef77a
jhreyes483 <jhreyes483@misena.edu.co>
2023-12-01 20:49:00 -0600

    se genera endpoint save-user

--- api-rest-node/controllers/user.js
+++ api-rest-node/controllers/user.js
@@ -1,6 +1,10 @@
 'use strict'
 
 var validator = require('validator');
+var bcrypt    = require('bcrypt-nodejs')
+
+var User      = require('../models/user');
+
 
 var controller = {
     probando: function(req, res){
@@ -18,12 +22,13 @@ var controller = {
         var params = req.body;
 
         var error = false;
+        var sendResponse = false;
         var msg   = '';
         // Validar los datos
-        var validate_name        = validator.isEmpty(params.name);
-        var validate_surname     = validator.isEmpty(params.surname);
-        var validate_emial       = (validator.isEmpty(params.email) || !validator.isEmail(params.email));
-        var validate_password    = validator.isEmpty(params.password);
+        var validate_name     = validator.isEmpty(params.name);
+        var validate_surname  = validator.isEmpty(params.surname);
+        var validate_emial    = (validator.isEmpty(params.email) || !validator.isEmail(params.email));
+        var validate_password = validator.isEmpty(params.password);
         //console.log(validate_name , validate_surname, validate_emial, validate_password);
         if(validate_name){
             error = true 
@@ -43,30 +48,76 @@ var controller = {
         }
         if( error ){
             return res.status(200).send({
-                message: "parametro incorrecto "+ msg,
+                status : 'error',
+                msg    : "parametro incorrecto "+ msg,
                 params
             });
         }
-
         // Crear objeto de usuario
+        var user = new User();
 
         // Asignar valores al usuario
+        user.name    = params.name;
+        user.surname = params.surname;
+        user.email   = params.email.toLowerCase();
+        user.password= params.password;
+        user.role    = 'ROLE_USER';
+        user.image   = null;
 
         // Comprobar si el usuario ya existe 
+        User.findOne({email: user.email}, (err, issetUser) =>{    
+            console.log('recarag---')
+    
+            if(err) {
+                return res.status(500).send({
+                    status : 'error',
+                    msg    : "Error al comprobar duplicidad de usuario",
+                    params
+                });
+            } 
 
-        // Si no existe, cifrar la contraseña y guradarlo
+            if(issetUser){ // ya se habia registrado
+                return res.status(400).send({
+                    status : 'error',
+                    msg    : "El usuario ya se habia restrado",
+                    params
+                });
+            }
+            
+            if(!issetUser){
+                bcrypt.hash(params.password, null,null,(err, hash) =>{
+                    user.password = hash; // encripta la contraseña
+                    user.save((err,userStored) =>{
+                        if(err) {
+                            return res.status(500).send({
+                                status : 'error',
+                                message: "Error al guardar el usuario",
+                                params
+                            });
+                        } 
+                        if(userStored){ // Devolver respuesta ok
+                            return res.status(200).send({
+                                status: 'success',
+                                msg: 'usuario creado',
+                                user: userStored
+                            })
 
-        //Devovler una respuesta
+                            /*
+                            return res.status(400).send({
+                                status : 'error',
+                                message: "El usuario no se ha guardado",
+                                userStored
+                            });
+                            */
+                        }
 
-
-       
-        return res.status(200).send({
-            message: "Registro de usuarios",
-            params
-
-        });
+                    }) // close save 
+                }); // close bcrypt
+            }    
+                
+        });// metodo find
     }
-
+    
 };
 
 
